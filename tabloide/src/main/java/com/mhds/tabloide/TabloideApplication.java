2243531c6bf15b04bdbb9b7706129d3c91a9858f
wallace.silva <gomesw@gmail.com>
2024-08-27 11:22:00 -0300

    first commit

---
+++ tabloide/src/main/java/com/mhds/tabloide/TabloideApplication.java
@@ -0,0 +1,13 @@
+package com.mhds.tabloide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabloideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabloideApplication.class, args);
+	}
+
+}
---
+++ tabloide/src/test/java/com/mhds/tabloide/TabloideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhds.tabloide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabloideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
